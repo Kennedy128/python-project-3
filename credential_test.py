098ca127d433f48c93a939982551a8586e1c3ed6
Santa claus <santa@northpole.com>
2020-04-20 12:42:46 +0300

    addition of comments on the credential test py

--- credential_test.py
+++ credential_test.py
@@ -5,14 +5,20 @@ from credentials import Credential
 class TestUser(unittest.TestCase):
    
     def setUp(self):
-        
+        '''
+        it ensure that the code is running in each instance of testing
+        '''
         self.new_credential = Credential("twitter","kennedymbithi12")
     def test_init(self):
-         
+        '''
+        it creates new instances of classes using the init method
+        '''
         self.assertEqual(self.new_credential.account_name,"twitter")
         self.assertEqual(self.new_credential.passwordkey,"kennedymbithi12")
     def test_save_credential(self):
-        
+        '''
+        it saves the addded creadential
+        '''
         self.new_credential.save_credential()
         self.assertEqual(len(Credential.credential_list),1)
     def test_display_credentials(self):
@@ -20,8 +26,8 @@ class TestUser(unittest.TestCase):
         self.assertEqual(Credential.display_credentials(),Credential.credential_list)
     def test_delete_credential(self):
         '''
-        test_delete_credential to see if we can remove a 
-        credential from credentials list
+        test_delete_credential it removes the inputed credential from the list 
+        
         '''
         self.new_credential.save_credential()
         test_credential = Credential("test","0893uhjnv")
